24445efa71f6b237544b071f464952628bb1eeaa
FriedSk8r <khelashvilivakho@gmail.com>
2023-07-21 10:28:41 +0400

    initial commit

---
+++ build/precache-manifest.7644939f5ef0b6865a2a30835c956141.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb6c58742286f53bf9ac6896c85e5b1",
+    "url": "/keeper-app-3/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/keeper-app-3/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "99d93bc91987c84a5b14",
+    "url": "/keeper-app-3/static/js/main.5d6a95d6.chunk.js"
+  },
+  {
+    "revision": "930b49ebbdef030d0402",
+    "url": "/keeper-app-3/static/js/runtime-main.e9991b7d.js"
+  }
+]);
